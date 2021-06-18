38f2891ec12b8c1bed25b540460e899d68573fb0
Matt Mastracci <matthew@mastracci.com>
2021-06-18 09:41:19 -0600

    Restore makefile, get make test working

---
+++ Makefile
@@ -0,0 +1,78 @@
+SRCDIR=src
+TESTDIR=test
+OUTDIR=target
+OBJDIR=$(OUTDIR)/obj
+DOS_OBJDIR=$(OBJDIR)/dos
+TEST_OBJDIR=$(OBJDIR)/test
+HOST_OBJDIR=$(OBJDIR)/host
+
+SOURCES=$(wildcard $(SRCDIR)/*.c)
+TEST_SOURCES=$(wildcard $(TESTDIR)/*.c) $(wildcard $(TESTDIR)/unity/*.c)
+TEST_LIB_OBJECTS=$(patsubst $(SRCDIR)/%.c,$(TEST_OBJDIR)/lib/%.o,$(SOURCES))
+TEST_OBJECTS=$(TEST_LIB_OBJECTS) $(patsubst $(TESTDIR)/%.c,$(TEST_OBJDIR)/test/%.o,$(TEST_SOURCES))
+
+TPUT_SGR0=$(shell tput sgr0 || echo '')
+TPUT_BOLD=$(shell tput bold || echo '')
+TPUT_DIM=$(shell tput dim || echo '')
+TPUT_BG_BLACK=$(shell tput setab 0 || echo '')
+TPUT_FG_GREEN=$(shell tput setaf 2 || echo '')
+
+define color_sh
+	printf "$(TPUT_SGR0)$(TPUT_FG_GREEN)[ %-5s ]$(TPUT_SGR0)$(TPUT_FG_GREEN)$(TPUT_BOLD) %-30s$(TPUT_SGR0) $(TPUT_DIM)(%s)$(TPUT_SGR0)\n" "$1" "$3" "$2"
+endef
+
+define color
+	@printf "$(TPUT_SGR0)$(TPUT_FG_GREEN)[ %-5s ]$(TPUT_SGR0)$(TPUT_FG_GREEN)$(TPUT_BOLD) %-30s$(TPUT_SGR0) $(TPUT_DIM)(%s)$(TPUT_SGR0)\n" "$1" "$3" "$2"
+endef
+
+WATCOM_MODEL=m
+WATCOM_CFLAGS=-march=i86 -Wall -Werror -std=c99 \
+	-fno-writable-strings -fnonconst-initializers -fshort-enum -ffunction-sections \
+	-mcmodel=$(WATCOM_MODEL) -O3 -frerun-optimizer -floop-optimize -funroll-loops -fno-stack-check
+TEST_CFLAGS=-std=c99 -fmacro-backtrace-limit=0 \
+	-Werror -Wpedantic -Wno-typedef-redefinition -Wno-c11-extensions -Wno-gnu-flexible-array-initializer -ftrapv \
+	-DIS_TEST -g -O0 \
+	-fsanitize=undefined -fsanitize=address -fsanitize=integer -fsanitize=bounds \
+	-fno-omit-frame-pointer
+HOST_CFLAGS=-std=c99 -fmacro-backtrace-limit=0 \
+	-Werror -Wpedantic -Wno-typedef-redefinition -Wno-c11-extensions -Wno-gnu-flexible-array-initializer -ftrapv \
+	-g -O0 \
+	-fsanitize=undefined -fsanitize=address -fsanitize=integer -fsanitize=bounds \
+	-fno-omit-frame-pointer
+
+.PHONY: test
+
+test: $(OUTDIR)/tests/test
+	$(call color,"TEST","host",$<)
+	@$(OUTDIR)/tests/test
+
+clean:
+	$(call color,"CLEAN","all",$(OUTDIR))
+	@rm -rf $(OUTDIR)
+
+$(SRCDIR)/kalos_lex.c: $(SRCDIR)/kalos_lex.re
+	$(call color,"re2c","all",$<)
+	@re2c -W -c --tags --no-debug-info $< -o $@
+
+$(SRCDIR)/kalos_string_format.c: $(SRCDIR)/kalos_string_format.re
+	$(call color,"re2c","all",$<)
+	@re2c -W -Wno-nondeterministic-tags -Wno-match-empty-string -c --tags --no-debug-info $< -o $@
+
+$(SRCDIR)/kalos_idl_parse.c: $(SRCDIR)/kalos_idl_parse.re
+	$(call color,"re2c","all",$<)
+	@re2c -W -c --tags --no-debug-info $< -o $@
+
+$(TEST_OBJDIR)/lib/%.o: $(SRCDIR)/%.c $(HEADERS)
+	$(call color,"CC","host",$<)
+	@mkdir -p $(dir $@)
+	@$(CC) $(TEST_CFLAGS) -c $< -o $@
+
+$(TEST_OBJDIR)/test/%.o: $(TESTDIR)/%.c $(TESTDIR)/*.h $(TESTDIR)/*.inc $(HEADERS) Makefile
+	$(call color,"CC","host",$<)
+	@mkdir -p $(dir $@)
+	@$(CC) $(TEST_CFLAGS) -Isrc -c $< -o $@
+
+$(OUTDIR)/tests/test: $(TEST_OBJECTS)
+	$(call color,"LINK","host",$@)
+	@mkdir -p $(dir $@)
+	@$(CC) $(TEST_CFLAGS) $(TEST_OBJECTS) -o $@
---
+++ test/main.c
@@ -0,0 +1,102 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "test.h"
+
+test_suite* suites[256] = {0};
+int test_suite_count = 0;
+
+FILE* log_stream;
+
+void register_suite(test_suite* current_suite) {
+    // printf("suite %s\n", current_suite->name);
+    suites[test_suite_count++] = current_suite;
+}
+
+void test_log(char* file, int line, const char* fmt, ...) {
+    va_list args;
+    static char print_buffer[2048];
+
+    va_start(args, fmt);
+    int written = vsnprintf(&print_buffer[0], sizeof(print_buffer), fmt, args);
+    va_end(args);
+
+    strcat(&print_buffer[0], "\n");
+    fprintf(log_stream, "%s:%d: ", file, line);
+    fwrite(&print_buffer[0], written + 1, 1, log_stream);
+    fflush(log_stream);
+}
+
+void test_log_direct(const char* fmt, ...) {
+    va_list args;
+    static char print_buffer[2048];
+
+    va_start(args, fmt);
+    int written = vsnprintf(&print_buffer[0], sizeof(print_buffer), fmt, args);
+    va_end(args);
+
+    strcat(&print_buffer[0], "\n");
+    fwrite(&print_buffer[0], written + 1, 1, log_stream);
+    fflush(log_stream);
+}
+
+int main(void) {
+    log_stream = fopen("/tmp/fdl-test.log", "wb");
+    int failures = 0;
+
+    for (int i = 0; i < test_suite_count; i++) {
+        UnityPrint(suites[i]->file);
+        UNITY_PRINT_EOL();
+        UNITY_PRINT_EOL();
+        UnityBegin(suites[i]->file);
+        for (int j = 0; j < suites[i]->test_count; j++) {
+            test_data* test_data = &suites[i]->tests[j];
+            Unity.CurrentTestName = test_data->name;
+            Unity.CurrentTestLineNumber = (UNITY_LINE_TYPE)test_data->line;
+            Unity.NumberOfTests++;
+
+            test_log_direct("");
+            test_log_direct("***********************************************************");
+            test_log_direct("* %s", test_data->name);
+            test_log_direct("***********************************************************");
+            test_log_direct("");
+
+            UNITY_CLR_DETAILS();
+            UNITY_EXEC_TIME_START();
+            if (TEST_PROTECT())
+            {
+                // setUp();
+                test_data->fn();
+            }
+            if (TEST_PROTECT())
+            {
+                // tearDown();
+            }
+            UNITY_EXEC_TIME_STOP();
+
+            if (Unity.CurrentTestIgnored) {
+                test_log_direct("IGNORED");                
+            } else if (Unity.CurrentTestFailed) {
+                test_log_direct("FAILED");
+            } else {
+                test_log_direct("PASSED");
+            }
+
+            UnityConcludeTest();
+        }
+        failures += UnityEnd();
+        UNITY_PRINT_EOL();
+    }
+
+    if (failures) {
+        UnityPrintNumber(failures);
+        UnityPrint(" Failure(s)");
+    } else {
+        UnityPrint("All tests passed!");
+    }
+    UNITY_PRINT_EOL();
+
+    fclose(log_stream);
+}
