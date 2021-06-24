d54163887709b1a7eb924e2b33fc4e35ce04f615
Matt Mastracci <matthew@mastracci.com>
2021-06-24 15:10:05 -0600

    Continue removing older props

--- src/_kalos_idl_compiler.c
+++ src/_kalos_idl_compiler.c
@@ -217,7 +217,6 @@ static void iter_function_arg(kalos_state* state, void* context, uint16_t index,
 static kalos_string kalos_idl_module_prefix(kalos_state* state) { return kalos_string_allocate(state, script_current_header->prefix_index ? kalos_module_get_string(script_modules, script_current_header->prefix_index) : "kalos_idl_"); }
 
 static kalos_string kalos_idl_export_name2(kalos_state* state, kalos_object_ref* o) { return kalos_string_allocate(state, kalos_module_get_string(script_modules, script_current_export->name_index)); }
-static kalos_int kalos_idl_function_id2(kalos_state* state, kalos_object_ref* o) { return script_current_function->binding.invoke_id; }
 static kalos_string kalos_idl_function_return_type2(kalos_state* state, kalos_object_ref* o) { return kalos_string_allocate(state, function_type_to_string(script_current_function->return_type)); }
 static kalos_object_ref kalos_idl_function_args2(kalos_state* state, kalos_object_ref* o) {
     kalos_int* index;
