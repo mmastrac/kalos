619e6fa8bc2af5cedb47fbbcf1ff03577113dd8b
Matt Mastracci <matthew@mastracci.com>
2022-09-14 15:25:14 -0600

    Stronger typing for kalos file constants

--- src/modules/kalos_module_file.h
+++ src/modules/kalos_module_file.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "../kalos_run.h"
 
+static const kalos_int KALOS_FILE_READ_ONLY = 1;
+static const kalos_int KALOS_FILE_WRITE_ONLY = 2;
+static const kalos_int KALOS_FILE_READ_WRITE = 3;
+static const kalos_int KALOS_FILE_CREATE = 4;
+static const kalos_int KALOS_FILE_TRUNCATE = 8;
+static const kalos_int KALOS_FILE_APPEND = 16;
+
 kalos_object_ref kalos_file_open(kalos_state* state, kalos_string* file_, kalos_int mode_);
 kalos_string kalos_file_readline(kalos_state* state, kalos_object_ref* object);
 kalos_string kalos_file_read_all(kalos_state* state, kalos_object_ref* object);
